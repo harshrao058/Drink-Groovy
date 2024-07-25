00c83d270d219ea55fab68903d8051cc6323b55a
harshrao26 <harshurao058@gmail.com>
2024-07-25 17:18:07 +0530

    sec last Marqq

--- src/Components/LeftToRightMarquee.jsx
+++ src/Components/LeftToRightMarquee.jsx
@@ -8,7 +8,7 @@ const LeftToRightMarquee = () => {
       <VelocityScroll
         text="Jucies Served with Love | 100% natural fruit juice | No added colors | no preservatives | "
         default_velocity={1}
-        className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
+        className="font-display text-center text-3xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
       />
       </div>
     </>
