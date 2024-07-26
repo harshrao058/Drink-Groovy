dc88ec45df60195a0e5f050605a2ada7d28ab523
harshrao26 <harshurao058@gmail.com>
2024-07-26 23:24:16 +0530

    Nav Color change on scroll

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,18 +1,36 @@
-import React from "react";
-import { FiSearch } from "react-icons/fi";
-import { LuShoppingCart } from "react-icons/lu";
-import { GrFavorite } from "react-icons/gr";
-import { FaRegUser } from "react-icons/fa6";
-import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import { LuShoppingCart } from 'react-icons/lu';
+import { GrFavorite } from 'react-icons/gr';
+import { FaRegUser } from 'react-icons/fa6';
+import { NavLink } from 'react-router-dom';
 
-import logo from "../../public/logo.png";
+import logo from '../../public/logo.png';
 
 const Nav = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="w-full z-50 fixed flex items-center justify-between lg:px-16 px-8 lg:py-8 py-4 h-24  text-black">
+    <div className={`w-full z-50 fixed flex items-center justify-between lg:px-16 px-8 lg:py-8 py-4 h-24 text-black nav-transition ${isScrolled ? 'bg-zinc-800 text-white transition-all ease-in-out'  : ''}`}>
       <div className="flex items-center gap-16">
         <img src={logo} alt="Logo" className="w-[8vw]" />
-        <div className="flex gap-[4vw] uppercase">
+        <div className="flex items-center gap-[4vw] uppercase">
           <NavLink
             to="/"
             className="cursor-pointer font-semibold text-sm text-white bg-zinc-800 px-2 py-1 hover:text-gray-400"
@@ -50,7 +68,7 @@ const Nav = () => {
         <FiSearch className="text-xl font-bold" />
         <LuShoppingCart className="text-xl" />
         <GrFavorite className="text-xl" />
-        <div className=" bg-zinc-800 p-2 text-white rounded-full">
+        <div className="bg-zinc-800 p-2 text-white rounded-full">
           <FaRegUser className="text-base" />
         </div>
       </div>
